27893eda72457670d161d961fb690009a52076a3
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-07-20 23:50:05 +0630

    zoom day-1

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+//string
+// $name = 'Hlaing Min Than';
+// $name = "Zaw Zaw";
+
+// //number
+// $name = 12; //int
+// $name = 12.4; //float
+
+
+$age = 100;
+$age = null;
+
+var_dump($age);
+
+
+
+
+//bolean
+// $name = true; //true,false
+
+
+// //array
+
+// //numeric array
+$names = ['mgmg', 'aung aung', 'kyaw kyaw', 'zaw zaw']; // length - 4 , $names[3]
+
+// //associated array
+$studentInfo = [
+    'name' => 'Hlaing Min Than',
+    'email' => 'hlaingminthan92@gmail.com'
+]; //2 , $studentInfo['name]
+
+// //arithmetic -> value
+// $total = 1000;
+
+// $total = $total - 1;
+
+// echo $total;
+// // $product1 = 1000;
+// $product2 = 2000;
+// $product3 = 3000;
+
+
+// $total += $product1; // $total = 1000
+// $total += $product3; // $total = 4000
+
+// $value = 10 + 3 - 2 * 4; // ????
+
+// echo $value;
+
+//comparison operator -> boolean
+//true , false
+
+// == , === (difference)
+
+// 1000 !== 1000;
